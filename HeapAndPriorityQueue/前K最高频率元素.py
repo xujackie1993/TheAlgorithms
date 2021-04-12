f50f3dc84baa7747afcc2d605de0d07b6566e4e7
xujackie <xuzj.com>
2021-04-12 23:39:10 +0800

    增加前k最高频率元素

--- HeapAndPriorityQueue/前K最高频率元素.py
+++ HeapAndPriorityQueue/前K最高频率元素.py
@@ -10,6 +10,52 @@ class Solution(object):
         heapq.heapify(heap)
         return [heapq.heappop(heap)[1] for _ in range(k)]
 
+# 最高频率单次
+    def top_k_frequent_word(self, A, k):
+        if k <= 0 or len(A) < k:
+            return []
+        d_fre = {}
+        for x in A:
+            num = d_fre.get(x, 0)
+            d_fre[x] = num + 1
+
+        heap = []
+        for k, v in d_fre.items():
+            heapq.heappush(heap, (v, k))
+            while len(heap) > k:
+                heapq.heappush(heap)
+
+        ans = []
+        while len(heap) > 0:
+            p = heapq.heappop(heap)
+            ans.append(p[1])
+        ans.reverse()
+        return ans
+
+    # 离原点最近的k个点
+    def k_close(self, A, k):
+        if k <= 0 or len(A) < k:
+            return []
+
+
+        def dist(t):
+            x = t[0] * t[0]
+            y = t[1] * t[1]
+            return x + y
+
+        heap = []
+        for i in range(A):
+            heapq.heappush(heap, (-dist(A[i]), k))
+            while len(heap) > k:
+                heapq.heappush(heap)
+
+        ans = []
+        while len(heap) > 0:
+            p = heapq.heappop(heap)
+            ans.append(p[1])
+        return ans
+
+
 if __name__ == "__main__":
 
     s = Solution()
